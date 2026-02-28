0df4b32ea57ad986f07aa9f6955e4b2088abde2e
poptart <404@poptart.dev>
2026-02-27 20:19:27 -0500

    bunch of things, cleaned up box formatting especially

--- js/bajablast.js
+++ js/bajablast.js
@@ -1,17 +1,56 @@
 // Baja Blast Functions
 
 // Get the current date object and format it
-const today = new Date();
-const formattedDate = today.toDateString(); 
+    const today = new Date();
+    const formattedDate = today.toDateString(); 
 
-// Return Variables to HTML
-// Inject the formatted date into the HTML element with id="displayDate"
-document.getElementById("displayDate").textContent = formattedDate; 
+    // Return Variables to HTML
+    document.getElementById("displayDate").textContent = formattedDate; 
 
 
 // Calculate number of baja blasts so far this year
-yearStart = new Date(new Date().getFullYear(), 0, 1);
-dayCount = daysPassed(today,yearStart);
+    yearStart = new Date(new Date().getFullYear(), 0, 1);
+    dayCount = daysPassed(today,yearStart);
 
-// Return Variables to HTML
-document.getElementById("daysYear").textContent = dayCount;
+    // Return Variables to HTML
+    document.getElementById("daysYear").textContent = dayCount;
+
+// Is Poptart having a baja blast?
+    const state = Math.round(Math.random());
+    let blast = "Poptart is experiencing Schrodinger's Baja Blast, please stand by.";
+    let blastImg = "/image/neocities.gif";
+    if (state == 0) {
+        blast = "Poptart is not having a Baja Blast";
+        blastImg = "/image/baja-sad-cat.gif";
+    }
+    else if (state == 1) {
+        blast = "Poptart is having a Baja Blast";
+        blastImg = "/image/baja-happy-cat.gif";
+
+    }
+    else {
+        blast = "Poptart is experiencing Schrodinger's Baja Blast, please stand by.";
+        blastImg = "/image/neocities.gif";
+    }
+
+    // blastImg = "/image/baja-sad-cat.gif";
+
+
+    // Return Variables to HTML
+    document.getElementById("isBlast").textContent = blast;
+    const blastElement = document.getElementById("blastImg");
+    blastElement.src = blastImg;
+    // .textContent = blastImg;
+    // isBlastImg.src = blastImg;
+
+
+// //     // 1. Define the new image source URL
+// // const newImgSrc = "images/landscape.jpg";
+
+// // 2. Get the image element by its ID
+// // const imageElement = document.getElementById("myImage");
+
+// // 3. Set the src property to the new source
+// imageElement.src = newImgSrc; //
+
+
