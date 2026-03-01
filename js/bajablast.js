1e75196b3b18c19b7c386974305eeefa8002c73e
Kelly Burghart <81436112+kburghart@users.noreply.github.com>
2026-02-28 19:36:31 -0500

    feed poptart a baja blast works!

--- js/bajablast.js
+++ js/bajablast.js
@@ -1,78 +1,61 @@
 // Baja Blast Functions
 
 // Get the current date object and format it
-    const today = new Date();
-    const formattedDate = today.toDateString(); 
+    // const today = new Date();
+    // const formattedDate = today.toDateString(); 
 
-    // Return Variables to HTML
-    document.getElementById("displayDate").textContent = formattedDate; 
+    // // Return Variables to HTML
+    // document.getElementById("displayDate").textContent = formattedDate; 
 
-
-// Calculate number of baja blasts so far this year
-    yearStart = new Date(new Date().getFullYear(), 0, 1);
-    dayCount = daysPassed(today,yearStart);
-
-    // Return Variables to HTML
-    // document.getElementById("daysYear").textContent = dayCount;
-
-
+// // Calculate number of baja blasts so far this year
+//     yearStart = new Date(new Date().getFullYear(), 0, 1);
+//     dayCount = daysPassed(today,yearStart);
 
 // Is Poptart having a baja blast?
-    const state = Math.round(Math.random());
-    let blast = "Poptart is experiencing Schrodinger's Baja Blast, please stand by.";
-    let blastImg = "/image/poptart.png";
-    if (state == 0) {
-        blast = "Poptart is not having a Baja Blast";
-        blastImg = "/image/baja-sad-cat.gif";
-    }
-    else if (state == 1) {
-        blast = "Poptart is having a Baja Blast";
-        blastImg = "/image/baja-happy-cat.gif";
-
-    }
-    else {
-        blast = "Poptart is experiencing Schrodinger's Baja Blast, please stand by.";
-        blastImg = "/image/poptart.png";
-    }
-
-    // Return Variables to HTML
-    document.getElementById("isBlast").textContent = blast;
-    const blastElement = document.getElementById("blastImg");
-    blastElement.src = blastImg;
-
-// Feed Baja Blast Function
-const smolButton = document.getElementById('interactive-button');
-
-interactiveButton.addEventListener('click', function() {
-    feedBajaBlast(smolButton);
-});
-
-function feedBajaBlast(){
-    addNumber = addNumber +1 
-    localStorage.setItem('counter', addNumber);
+const state = Math.round(Math.random());
+let blast = "Poptart is experiencing Schrodinger's Baja Blast, please stand by.";
+let blastImg = "/image/poptart.png";
+if (state == 0) {
+    blast = "Poptart is not having a Baja Blast";
+    blastImg = "/image/baja-sad-cat.gif";
+}
+else if (state == 1) {
+    blast = "Poptart is having a Baja Blast";
+    blastImg = "/image/baja-happy-cat.gif";
+}
+else {
+    blast = "Poptart is experiencing Schrodinger's Baja Blast, please stand by.";
+    blastImg = "/image/poptart.png";
 }
 
-// function feedBajaBlast() {
-//     // const bajaCount = document.createElement('div');
-//     // bajaCount.classList.add('bajaCount');
+// Return Variables to HTML
+document.getElementById("isBlast").textContent = blast;
+const blastElement = document.getElementById("blastImg");
+blastElement.src = blastImg;
 
-//     // const increment = document.getElementById('increment')
-//     let counter = document.getElementById('counter')
+// Feed Baja Blast Function
+// Initialize count
+const x = document.getElementById("result");
+if (localStorage.clickcount) {
+    x.innerHTML = Number(localStorage.clickcount);
+}
+else {
+    x.innerHTML = Number(1);
+}
 
-//     increment.addEventListener('click', () => {
-//     increment.disabled = false
-
-//     fetch('./increment.php')
-//         .then(response => response.text())
-//         .then(count => {
-//         counter.textContent = count
-//         })
-//         .catch(error => {
-//         counter.textContent = error
-//         })
-//         .finally(() => {
-//         increment.disabled = false
-//         })
-//     })
-    
-    // }
+// Increment count function
+function clickCounter() {
+    const x = document.getElementById("result");
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount)+1;
+            } 
+        else {
+            localStorage.clickcount = 1;
+            }
+        x.innerHTML = localStorage.clickcount;
+        } 
+    else {
+        x.innerHTML = "Sorry, no Web storage support!";
+        }
+    }
--- js/visitcount.js
+++ js/visitcount.js
@@ -1,9 +1,32 @@
+// From https://dannarchy.com/tut/tut_002
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        var site_data = JSON.parse(this.responseText);    }
-  };
+      // DO SOMETHING
+      var site_data = JSON.parse(this.responseText);
+        // visit count
+        // document.write(site_data.result)
+        // document.getElementById("hitcount").innerHTML = site_data.info.views;
+        var num_arr = site_data.info.views.toString().split("");
+        var num_str = "";
+        for (i = 0; i < num_arr.length; i++) {
+        num_str += num_arr[i];
+        if ( (num_arr.length-1 - i) % 3 == 0 && (num_arr.length-1 - i) != 0 ) {num_str += ",";}
+        }
+
+        document.getElementById("hitcount").innerHTML = num_str;
+
+        // last updated
+        // var date_str = site_data.info.last_updated;
+        // document.getElementById("lastupdate").innerHTML = date_str;
+
+        var date_obj = new Date(site_data.info.last_updated);
+        var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        // document.getElementById("lastupdate").innerHTML = days[date_obj.getDay()] + " " + months[date_obj.getMonth()] + " " + date_obj.getDate() + ", " + date_obj.getFullYear();
+        document.getElementById("lastupdate").innerHTML = months[date_obj.getMonth()] + " " + date_obj.getDate() + ", " + date_obj.getFullYear();
+    }
+    };
   xhttp.open("GET", "https://weirdscifi.ratiosemper.com/neocities.php?sitename=poptart404", true);
   xhttp.send();
 
-  document.getElementById("hitcount").innerHTML = site_data.info.views;
