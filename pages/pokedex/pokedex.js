bda5d43b3a9de3b6410314bc2a759455ef6b7947
Kelly Burghart <81436112+kburghart@users.noreply.github.com>
2026-03-04 01:07:02 -0500

    pokedex selection functionality in progress. json file structure is set up and working on dropdown. may need dropdown with confirm button.

--- pages/pokedex/pokedex.js
+++ pages/pokedex/pokedex.js
@@ -1,29 +1,65 @@
-const searchBtn = document.getElementById('search-btn'); // search button
-const inputField = document.getElementById('name-input'); // search field input
-const nameScreen = document.getElementById('name-screen'); //name-screen
-const imageScreen = document.getElementById('main-screen'); // image screen
-const aboutScreen = document.getElementById('about-screen'); // about-text screen
-const typeScreen = document.getElementById('type-screen'); // type screen
-const idScreen = document.getElementById('id-screen'); // spices screen
+const dbParam = JSON.stringify({table:"",limit:200});
+const xmlhttpItem = new XMLHttpRequest();
+var itemID = 0;
+xmlhttpItem.onload = function() {
+    // const myArr = JSON.parse(this.responseText);
+    // const myObj2 = myArr[1];
+    let x=0;
+    const myObj2 = JSON.parse(this.responseText);
+    let text = "<select class='blueButtonLong' id='demo3'>"
+    for (let x in myObj2) {
+        text += "<option>" + x + ". " + myObj2[x].name + "</option>";
+    }
+    text += "</select>"
+    document.getElementById("demo3").innerHTML = text;
+    // document.getElementById("demoID").innerHTML = x;
+    itemID = x;
+        console.log(itemID);
 
-const getPokemonData = (pokemon) => {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-    .then((response) => response.json())
-    .then((data) => {
-      let id = ('00' + data.id).slice(-3);
-      imageScreen.style.backgroundImage = `url('https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png')`;
-      nameScreen.innerHTML = data.name;
-      typeScreen.innerHTML = data.types[0].type.name;
-      idScreen.innerHTML = `#${data.id}`;
-      aboutScreen.innerHTML = `Height: ${data.height * 10}cm Weight: ${
-        data.weight / 10
-      }kg`;
-      inputField.value = '';
-    });
-};
+    }
+xmlhttpItem.open("GET", "data.json", true);
+xmlhttpItem.send();
 
-inputField.addEventListener(
-  'keydown',
-  (event) => event.key === 'Enter' && searchBtn.click()
-);
-searchBtn.addEventListener('click', () => getPokemonData(inputField.value));
+
+
+/* When the user selects an option, update the display fields for that item */
+function myFunction() {
+  document.getElementById("myDropdown").classList.replace();// toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onselectionchange = function(e) {
+  if (!e.target.matches('.blueButtons1')) {
+    updateScreen(); 
+    console.log(itemID);
+
+    // document.getElementById("demoID").innerHTML = x;
+    // var myDropdown = document.getElementById("myDropdown");
+    //     if (myDropdown.classList.contains('show')) {
+    //       myDropdown.classList.remove('show');
+    //   }
+  }
+}
+
+    function loadDetails() {
+      const xmlhttpDetails = new XMLHttpRequest();
+      const myItem = JSON.parse(this.responseText);
+      // let textName = myObj[itemID].name;
+      // let textFound = myObj[itemID].found;
+      // let textSize = myObj[itemID].size;
+      // let textURL = myObj[itemID].url;
+
+      let textName = myItem[0].name;
+      let textFound = myItem[0].found;
+      let textSize = myItem[0].size;
+      let textURL = myItem[0].url;
+
+      document.getElementById("itemName").innerHTML = textName;
+      document.getElementById("itemFound").innerHTML = textFound;
+      document.getElementById("itemSize").innerHTML = textSize;
+      document.getElementById("itemURL").innerHTML = textURL;
+
+      // console.log(itemID);
+      xmlhttpDetails.open("GET", "data.json", true);
+      xmlhttpDetails.send();
+      }//}
